b4bf054101f7546d1fc1ac9210be2c6632c8d512
GerryMON <mironova_plus@mail.ru>
2024-10-23 19:58:41 +0300

    Add files via upload

---
+++ Destroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // going to remove cube from the plane
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
